70552bc7aed1a016c5c503565a3a1da6563b9177
Xopek1337 <83090357+Xopek1337@users.noreply.github.com>
2021-07-15 19:53:39 +0300

    Add files via upload

--- SolarSystem.cpp
+++ SolarSystem.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
@@ -13,6 +15,9 @@ class Planet
 public:
 	Texture texture;
 	Sprite sprite;
+	Planet()
+	{
+	}
 
 	Planet(Image& image, double radius, double orbit, double centerx, double centery, double a)
 	{
@@ -35,14 +40,24 @@ public:
 		y = orbit * sin(dx) + centery;
 		sprite.setPosition(x, y);
 		dx += a;
+		if (dx > 2 * M_PI - a)
+			dx = 0;
 	}
 	double getAngle()
 	{
 		return dx;
 	}
-	void setAngle(double angle) 
+	void setAngle(double angle)
 	{
 		dx = angle;
+	}
+	double getx()
+	{
+		return x;
+	}
+	double gety()
+	{
+		return y;
 	}
 protected:
 	double x = 0;
@@ -54,18 +69,48 @@ protected:
 	double centerx;
 	double centery;
 };
+class sputnik :public Planet
+{
+public:
+	sputnik(Image& image, double radius, double orbit, double centerx, double centery, double a)
+	{
+		sprite.setOrigin(sf::Vector2f(image.getSize().x / 2, image.getSize().y / 2));
+
+		this->a = a;
+		this->centerx = centerx;
+		this->centery = centery;
+		this->radius = radius;
+		this->orbit = orbit;
+
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setScale(radius, radius);
+	}
+	void update(double cx,double cy)
+	{
+		x = orbit * cos(dx) + cx;
+		y = orbit * sin(dx) + cy;
+		sprite.setPosition(x, y);
+		dx += a;
+		if (dx > 2 * M_PI - a)
+			dx = 0;
+	}
+};
 class SolarSystem
 {
 public:
 	SolarSystem()
 	{
-
 	}
 	void addplanet(Planet planet)
 	{
 		v.push_back(planet);
 	}
-	void draw(RenderWindow &window,View view,Vector2f center)
+	void addsp(sputnik sp)
+	{
+		s.push_back(sp);
+	}
+	void draw(RenderWindow& window, View view, Vector2f center)
 	{
 		while (window.isOpen())
 		{
@@ -76,9 +121,7 @@ public:
 				{
 					window.close();
 				}
-
 			}
-
 			if (event.type == sf::Event::MouseWheelScrolled)
 			{
 				if (event.mouseWheelScroll.delta < 0)
@@ -101,11 +144,18 @@ public:
 			{
 				v[i].update();
 			}
-
+			for (int i = 0; i < s.size(); i++)
+			{
+				s[i].update(v[1].getx(),v[1].gety());
+			}
 			window.clear();
-			for (int i = 0;i<v.size();i++)
+			for (int i = 0; i < v.size(); i++)
 			{
 				window.draw(v[i].sprite);
+			}
+			for (int i = 0; i < s.size(); i++)
+			{
+				window.draw(s[i].sprite);
 			}
 			window.display();
 		}
@@ -122,10 +172,14 @@ public:
 				{
 					out << v[i].getAngle() << std::endl;
 				}
+				for (int i = 0; i < s.size(); i++)
+				{
+					out << s[i].getAngle() << std::endl;
+				}
 			}
 			else
 			{
-				for (int i = 0; i < v.size(); i++)
+				for (int i = 0; i < v.size()+s.size(); i++)
 				{
 					out << 0 << std::endl;
 				}
@@ -135,17 +189,26 @@ public:
 	void setCFG()
 	{
 		std::ifstream in("cfg.txt");
-		std::vector<int> inputData;
 		std::string line;
 		int i = 0;
+		int d = 0;
 		while (std::getline(in, line)) {
 			int temp = std::stoi(line);
-			v[i].setAngle(temp);
-			i++;
+			if (i < v.size())
+			{
+				v[i].setAngle(temp);
+				i++;
+			}
+			else
+			{
+				s[d].setAngle(temp);
+				d++;
+			}
 		}
 	}
 protected:
 	std::vector<Planet> v;
+	std::vector<sputnik> s;
 };
 int main()
 {
@@ -158,25 +221,35 @@ int main()
 	Image vImage;
 	Image sImage;
 	Image mImage;
+	Image marsImage;
+	Image moonImage;
 
 	eImage.loadFromFile("earth_PNG17.png");
 	vImage.loadFromFile("venus.png");
 	sImage.loadFromFile("sun.png");
 	mImage.loadFromFile("mercury.png");
-
-	Planet earth(eImage, 0.1, 225, center.x, center.y, 0.001);
+	marsImage.loadFromFile("mars.png");
+	moonImage.loadFromFile("moon.png");
+	
 	Planet sun(sImage, 0.08, 0, center.x, center.y, 0);
-	Planet venus(vImage, 0.02, 175, center.x, center.y, 0.0013);
-	Planet mercury(vImage, 0.015, 125, center.x, center.y, 0.0015);
+	Planet mars(marsImage, 0.03, 300, center.x, center.y, 0.0009);
+	Planet earth(eImage, 0.1, 230, center.x, center.y, 0.001);
+	Planet venus(vImage, 0.02, 155, center.x, center.y, 0.0013);
+	Planet mercury(mImage, 0.015, 100, center.x, center.y, 0.0015);
+	sputnik moon(moonImage, 0.007, 25, center.x, center.y, 0.002);
 
 	SolarSystem SS;
-	SS.addplanet(earth);
+
 	SS.addplanet(sun);
+	SS.addplanet(earth);
 	SS.addplanet(venus);
 	SS.addplanet(mercury);
+	SS.addplanet(mars);
+
+	SS.addsp(moon);
 
 	SS.setCFG();
-	
+
 	while (window.isOpen())
 	{
 		SS.draw(window, view, center);
